bd09d555ab0b0f07ae8bb73cf043e7c29a76deb2
Andrey Chausov <dxahtepb@gmail.com>
2019-12-06 18:13:21 +0300

    Lang: (refactor) use short expression-function syntax

--- src/main/kotlin/com/chausov/kotlisp/lang/Types.kt
+++ src/main/kotlin/com/chausov/kotlisp/lang/Types.kt
@@ -16,23 +16,21 @@ open class LispSequence protected constructor() : LispType {
 interface LispHashable: LispType
 
 class LispVector: LispSequence() {
-    override fun toString(): String {
-        return children.joinToString(
+    override fun toString(): String =
+        children.joinToString(
             separator=" ",
             prefix="[",
             postfix="]"
         )
-    }
 }
 
 class LispList: LispSequence() {
-    override fun toString(): String {
-        return children.joinToString(
+    override fun toString(): String =
+        children.joinToString(
             separator=" ",
             prefix="(",
             postfix=")"
         )
-    }
 }
 
 class LispHashMap: LispType {
@@ -42,45 +40,43 @@ class LispHashMap: LispType {
         map[key] = value
     }
 
-    fun get(key: LispHashable): LispType? {
-        return map[key]
-    }
+    fun get(key: LispHashable): LispType? = map[key]
 
-    override fun toString(): String {
-        return map.entries.joinToString(separator = " ", prefix = "{", postfix = "}", transform = {
+    override fun toString(): String =
+        map.entries.joinToString(
+            separator = " ",
+            prefix = "{",
+            postfix = "}",
+            transform = {
                 entry -> "${entry.key} ${entry.value}"
-        })
-    }
+            }
+        )
 }
 
 class LispText(val text: String): LispType {
-    override fun toString(): String {
-        return text
-    }
+    override fun toString(): String = text
 }
 
 class LispString(text: String): LispHashable {
     private val text: String = unescape(text)
 
-    private fun unescape(str: String): String {
-        return StringEscapeUtils.unescapeJava(str)
-    }
+    private fun unescape(str: String): String = StringEscapeUtils.unescapeJava(str)
 
     override fun hashCode(): Int = text.hashCode()
 
-    override fun equals(other: Any?): Boolean = if (other is LispString) text == other.text else false
+    override fun equals(other: Any?): Boolean =
+        other is LispString && text == other.text
 
-    override fun toString(): String {
-        return "\"$text\""
-    }
+    override fun toString(): String = "\"$text\""
 }
 
 class LispKeyword(private val text: String): LispHashable {
-    fun getText() = text
+    fun getText(): String = text
 
     override fun hashCode(): Int = text.hashCode()
 
-    override fun equals(other: Any?): Boolean = other is LispKeyword && other.text == text
+    override fun equals(other: Any?): Boolean =
+        other is LispKeyword && other.text == text
 
     override fun toString(): String = ":$text"
 }
