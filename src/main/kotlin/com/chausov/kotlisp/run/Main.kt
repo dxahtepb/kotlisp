d7ce24f723a7c8dbee53655b6d6bfa167ac927f9
Andrey Chausov <dxahtepb@gmail.com>
2019-12-07 19:29:50 +0300

    Run: add Lisp language defined (not x) function

--- src/main/kotlin/com/chausov/kotlisp/run/Main.kt
+++ src/main/kotlin/com/chausov/kotlisp/run/Main.kt
@@ -7,26 +7,34 @@ import java.io.EOFException
 
 fun lispRead(): LispType {
     print("> ")
-    val tokens = lispTokenize(readLine() ?: throw EOFException())
+    val str = readLine() ?: throw EOFException()
+    return lispParse(str)
+}
+
+fun lispParse(str: String): LispType {
+    val tokens = lispTokenize(str)
     return LispParser().parse(tokens)
 }
 
-fun lispEval(ast: LispType): LispType {
-    return LispDefaultEvaluateProcessor().eval(ast, DEFAULT_ENV)
+fun lispEval(ast: LispType, env: Environment): LispType {
+    return LispDefaultEvaluateProcessor().eval(ast, env)
 }
 
 fun lispPrint(ast: LispType) {
     Printer.printAst(ast)
 }
 
-fun rep() {
-    lispPrint(lispEval(lispRead()))
+fun rep(env: Environment) {
+    lispPrint(lispEval(lispRead(), env))
 }
 
 fun repl() {
+    val env = preRunCommands(listOf(
+        "(def! not (fn* (a) (if a false true)))"
+    ))
     while (true) {
         try {
-            rep()
+            rep(env)
         } catch (ex: EOFException) {
             return
         } catch (ex: Exception) {
@@ -35,6 +43,15 @@ fun repl() {
     }
 }
 
+fun preRunCommands(commands: List<String>): Environment {
+    val env = Environment(DEFAULT_ENV)
+    for (command in commands) {
+        lispEval(lispParse(command), env)
+    }
+    return env
+}
+
+//todo: add TravisCI builds for tests
 fun main(args: Array<String>) {
     println("Kotlisp interpreter")
     repl()
